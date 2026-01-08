2f3aeda2552ffef04f41346c64ddd6c4b4f93ff5
Nicolas <nikkaloo45@gmail.com>
2026-01-08 10:30:00 +0100

    Server.js creato e connesso a MongoDB

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.use(cors());
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('Database connesso')).catch(err => console.log(err));
+app.get('/', (req, res) => res.send('Server in esecuzione'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server in ascolto sulla porta ${PORT}`));
