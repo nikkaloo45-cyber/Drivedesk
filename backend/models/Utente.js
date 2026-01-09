7896410c94395d8041afef3bd09a2cd79f7cd4ce
Nicolas <nikkaloo45@gmail.com>
2026-01-09 11:00:00 +0100

    Risolto problema middleware Utente e creato Utente di prova nel database

--- backend/models/Utente.js
+++ backend/models/Utente.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const utenteSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    ruolo: { type: String, default: 'Manager' }
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    ruolo: {type: String, default: 'Manager'}
 });
-utenteSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-})
+    utenteSchema.pre('save', async function() {
+        if (!this.isModified('password')) return;
+        this.password = await bcrypt.hash(this.password, 10);
+    });
 module.exports = mongoose.model('Utente', utenteSchema);
